f7615544c29a9c74ef7d43fbb90fc5c749194b22
langfalvidavid <71819885+langfalvidavid@users.noreply.github.com>
2023-10-11 19:38:05 +0200

    vercel

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ["szakdoga-frontend-8o775g64r-langfalvi-davids-projects.vercel.app"],
+    origin: ["szakdoga-frontend.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true
 }))
