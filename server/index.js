023aee141eabc514f64d514c0accbab53382a52b
langfalvidavid <71819885+langfalvidavid@users.noreply.github.com>
2023-10-12 07:52:28 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://szakdoga-frontend.vercel.app");
+    res.setHeader("Access-Control-Allow-Origin", "https://szakdoga-frontend.vercel.app", "https://szakdoga-backend.vercel.app");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
