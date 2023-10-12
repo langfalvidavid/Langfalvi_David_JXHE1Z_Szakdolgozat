de60e9e001105852131f9b2d0e7eb60f6520f029
langfalvidavid <71819885+langfalvidavid@users.noreply.github.com>
2023-10-12 08:29:26 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,12 +14,12 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express()
 app.use(express.json())
 
-//app.use((req, res, next) => {
-//    res.setHeader("Access-Control-Allow-Origin", "https://szakdoga-frontend.vercel.app", "https://szakdoga-backend.vercel.app");
-//    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-//    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//    next();
-//  })
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
 
 app.use(cors({
     origin: ["https://szakdoga-frontend.vercel.app"],
