71b945d96a89a5f339ee9ab6ce1fe9f3d9993055
langfalvidavid <71819885+langfalvidavid@users.noreply.github.com>
2023-10-12 10:00:02 +0200

    Update Home.jsx

--- frontend/React/src/Home.jsx
+++ frontend/React/src/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     axios.defaults.withCredentials = true
     
     useEffect(()=>{
-        axios.get("https://szakdoga-backend.vercel.app")
+        axios.get("https://szakdolgozat-z4nt.onrender.com")
         .then(res => {console.log(res)
         if(res.data !== 'Sikeres bejelentkezés!'){
             navigate('/login')
