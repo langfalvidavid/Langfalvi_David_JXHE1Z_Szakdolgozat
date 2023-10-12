29b4c769210013420ed401f74f183098857c3517
langfalvidavid <71819885+langfalvidavid@users.noreply.github.com>
2023-10-12 07:27:28 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(cors({
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://szakdoga-frontend.vercel.app");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
