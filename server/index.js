dd133ff41fbbd25cddf6fd12211be165f88adcea
langfalvidavid <71819885+langfalvidavid@users.noreply.github.com>
2023-10-12 07:32:46 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,12 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express()
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://szakdoga-frontend.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
 
 app.use(cors({
     origin: ["https://szakdoga-frontend.vercel.app"],
@@ -22,12 +28,6 @@ app.use(cors({
 }))
 app.use(cookieParser())
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://szakdoga-frontend.vercel.app");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  })
 mongoose.connect(process.env.MONGODB)
 
 const verifyUser = (req, res, next) =>{
