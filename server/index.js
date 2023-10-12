6b2636c0ba2371c5a276d4a9eeed5d47c880eab7
langfalvidavid <71819885+langfalvidavid@users.noreply.github.com>
2023-10-12 09:53:33 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 //  })
 
 app.use(cors({
-    origin: "https://szakdoga-frontend.vercel.app",
+    origin: "https://szakdoga-frontend.vercel.app/",
  //   methods: ["GET", "POST"],
     credentials: true
 }))
