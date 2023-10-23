cda9d7918ecd753aa8e1c4ecda228e888eac1b6e
langfalvidavid <71819885+langfalvidavid@users.noreply.github.com>
2023-10-23 12:44:33 +0200

    asd

--- frontend/React/src/Home.jsx
+++ frontend/React/src/Home.jsx
@@ -4,14 +4,9 @@ import {Link, useNavigate} from 'react-router-dom'
 import Navbar from './Navbar';
 
 const Home = () => {
-    const navigate = useNavigate()
     axios.defaults.withCredentials = true
     
-    useEffect(()=>{
-        axios.get("https://szakdolgozat-z4nt.onrender.com")
-        .then(res => {console.log(res)})
-        .catch(err => console.log(err))
-    }, [])
+    
     return (
         
             <div className='vw-100 vh-100' style={{backgroundImage : "linear-gradient(to left bottom, #11b6f0, #00c1ea, #00cad5, #00d1b5, #08d48d)"}}> 
