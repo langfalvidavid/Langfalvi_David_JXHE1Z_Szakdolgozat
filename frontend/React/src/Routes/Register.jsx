6c2f5c7c6e015834615f7517cd0de7150a1c373f
langfalvidavid <71819885+langfalvidavid@users.noreply.github.com>
2024-03-09 15:51:07 +0100

    sss

--- frontend/React/src/Routes/Register.jsx
+++ frontend/React/src/Routes/Register.jsx
@@ -69,7 +69,7 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import { BrowserRouter as Router, useHistory } from 'react-router-dom';
 
 const Register = () => {
     const [email, setEmail] = useState('');
@@ -99,35 +99,37 @@ const Register = () => {
     };
 
     return (
-        <div style={{ backgroundImage: "linear-gradient(to left bottom, #11b6f0, #00c1ea, #00cad5, #00d1b5, #08d48d)" }}>
+        <Router>
+            <div style={{ backgroundImage: "linear-gradient(to left bottom, #11b6f0, #00c1ea, #00cad5, #00d1b5, #08d48d)" }}>
 
-            <div className='d-flex vh-100 w-50 m-auto border-danger align-items-center justify-content-center'>
+                <div className='d-flex vh-100 w-50 m-auto border-danger align-items-center justify-content-center'>
 
-                <form onSubmit={handleSubmit} className='w-50 bg-dark needs-validation rounded-4 novalidate'>
-                    <h1 className='text-light w-100 text-center mt-3'>Regisztráció</h1>
+                    <form onSubmit={handleSubmit} className='w-50 bg-dark needs-validation rounded-4 novalidate'>
+                        <h1 className='text-light w-100 text-center mt-3'>Regisztráció</h1>
 
-                    <div className='mb-3 mt-5 has-validation ' >
-                        <input type='email' className='form-control text-center w-75 m-auto' id='email' placeholder='Email' autoComplete='off' required onChange={(e) => { setEmail(e.target.value) }}></input>
-                    </div>
+                        <div className='mb-3 mt-5 has-validation ' >
+                            <input type='email' className='form-control text-center w-75 m-auto' id='email' placeholder='Email' autoComplete='off' required onChange={(e) => { setEmail(e.target.value) }}></input>
+                        </div>
 
-                    <div className='mb-3 has-validation '>
-                        <input type='text' className='form-control text-center w-75 m-auto' id='username' placeholder='Felhasználónév' autoComplete='off' required onChange={(e) => { setUsername(e.target.value) }}></input>
-                    </div>
+                        <div className='mb-3 has-validation '>
+                            <input type='text' className='form-control text-center w-75 m-auto' id='username' placeholder='Felhasználónév' autoComplete='off' required onChange={(e) => { setUsername(e.target.value) }}></input>
+                        </div>
 
-                    <div className='mb-3 has-validation '>
-                        <input type='password' className='form-control text-center w-75 m-auto' id='password' placeholder='Jelszó' autoComplete='off' required onChange={(e) => { setPassword(e.target.value) }}></input>
-                    </div>
+                        <div className='mb-3 has-validation '>
+                            <input type='password' className='form-control text-center w-75 m-auto' id='password' placeholder='Jelszó' autoComplete='off' required onChange={(e) => { setPassword(e.target.value) }}></input>
+                        </div>
 
-                    <div className='has-validation'>
-                        <input type='password' className='form-control text-center w-75 m-auto' id='password2' placeholder='Jelszó ismét' autoComplete='off' required onChange={(e) => { setPassword2(e.target.value) }}></input>
-                        <p className={errorMsg === '' && password === password2 ? 'text-transparent' : 'd-block text-danger text-center mt-3 mb-0'}>{(errorMsg === '') && (password !== password2) ? 'A jelszavak nem egyeznek!' : errorMsg}</p>
-                    </div>
-                    <div className='text-center'>
-                        <button type='submit' className='btn btn-info w-50 m-auto text-center rounded-4 text-dark mb-5'>Regisztráció</button>
-                    </div>
-                </form>
+                        <div className='has-validation'>
+                            <input type='password' className='form-control text-center w-75 m-auto' id='password2' placeholder='Jelszó ismét' autoComplete='off' required onChange={(e) => { setPassword2(e.target.value) }}></input>
+                            <p className={errorMsg === '' && password === password2 ? 'text-transparent' : 'd-block text-danger text-center mt-3 mb-0'}>{(errorMsg === '') && (password !== password2) ? 'A jelszavak nem egyeznek!' : errorMsg}</p>
+                        </div>
+                        <div className='text-center'>
+                            <button type='submit' className='btn btn-info w-50 m-auto text-center rounded-4 text-dark mb-5'>Regisztráció</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        </Router>
     );
 };
 
