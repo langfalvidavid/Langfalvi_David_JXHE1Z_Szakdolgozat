349903975142a6899628e0028a1d577f934e9955
langfalvidavid <71819885+langfalvidavid@users.noreply.github.com>
2024-03-09 15:55:35 +0100

    s

--- frontend/React/src/Routes/Register.jsx
+++ frontend/React/src/Routes/Register.jsx
@@ -69,7 +69,7 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
     const [email, setEmail] = useState('');
@@ -77,7 +77,7 @@ const Register = () => {
     const [password, setPassword] = useState('');
     const [password2, setPassword2] = useState('');
     const [errorMsg, setErrorMsg] = useState('');
-    const history = useHistory();
+    const navigate = useNavigate();
 
     axios.defaults.withCredentials = true;
 
@@ -89,7 +89,7 @@ const Register = () => {
                 console.log(res);
                 if (res.data === 'Felhasználó létrehozva') {
                     setTimeout(() => {
-                        history.push('/login'); // Átirányítás a '/login' útvonalra
+                        navigate('/login');
                     }, 3000);
                 } else {
                     return res.data;
@@ -100,26 +100,20 @@ const Register = () => {
 
     return (
         <div style={{ backgroundImage: "linear-gradient(to left bottom, #11b6f0, #00c1ea, #00cad5, #00d1b5, #08d48d)" }}>
-
             <div className='d-flex vh-100 w-50 m-auto border-danger align-items-center justify-content-center'>
-
                 <form onSubmit={handleSubmit} className='w-50 bg-dark needs-validation rounded-4 novalidate'>
                     <h1 className='text-light w-100 text-center mt-3'>Regisztráció</h1>
-
                     <div className='mb-3 mt-5 has-validation ' >
-                        <input type='email' className='form-control text-center w-75 m-auto' id='email' placeholder='Email' autoComplete='off' required onChange={(e) => { setEmail(e.target.value) }}></input>
+                        <input type='email' className='form-control text-center w-75 m-auto' id='email' placeholder='Email' autoComplete='off' required onChange={(e) => setEmail(e.target.value)} />
                     </div>
-
                     <div className='mb-3 has-validation '>
-                        <input type='text' className='form-control text-center w-75 m-auto' id='username' placeholder='Felhasználónév' autoComplete='off' required onChange={(e) => { setUsername(e.target.value) }}></input>
+                        <input type='text' className='form-control text-center w-75 m-auto' id='username' placeholder='Felhasználónév' autoComplete='off' required onChange={(e) => setUsername(e.target.value)} />
                     </div>
-
                     <div className='mb-3 has-validation '>
-                        <input type='password' className='form-control text-center w-75 m-auto' id='password' placeholder='Jelszó' autoComplete='off' required onChange={(e) => { setPassword(e.target.value) }}></input>
+                        <input type='password' className='form-control text-center w-75 m-auto' id='password' placeholder='Jelszó' autoComplete='off' required onChange={(e) => setPassword(e.target.value)} />
                     </div>
-
                     <div className='has-validation'>
-                        <input type='password' className='form-control text-center w-75 m-auto' id='password2' placeholder='Jelszó ismét' autoComplete='off' required onChange={(e) => { setPassword2(e.target.value) }}></input>
+                        <input type='password' className='form-control text-center w-75 m-auto' id='password2' placeholder='Jelszó ismét' autoComplete='off' required onChange={(e) => setPassword2(e.target.value)} />
                         <p className={errorMsg === '' && password === password2 ? 'text-transparent' : 'd-block text-danger text-center mt-3 mb-0'}>{(errorMsg === '') && (password !== password2) ? 'A jelszavak nem egyeznek!' : errorMsg}</p>
                     </div>
                     <div className='text-center'>
@@ -132,4 +126,5 @@ const Register = () => {
 };
 
 export default Register;
+
 
